c93a6f37b12204451866eda9258822d6b4e1f678
anompermadi <anom.permadi@outlook.com>
2022-04-18 15:21:05 +0700

    module object

---
+++ module/scripts/module_object.js
@@ -0,0 +1,14 @@
+export const firstName = "Arsenius";
+export const middleName = "Anom";
+export const lastName = "Permadi";
+
+export function hello(){
+    console.info("Hello from bro");
+}
+
+export class Person {
+    constructor(){
+        this.name = "Mona";
+    }
+}
+
